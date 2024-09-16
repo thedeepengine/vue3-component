5b418eda60fb29d06bfeae249e61f87d5b2dd33a
pablo <paul7junior@gmail.com>
2024-09-15 19:43:23 -0600

    consolidation

--- dimStore.js
+++ dimStore.js
@@ -3,6 +3,7 @@ import { ref, onMounted, watch } from "vue";
 import { defineStore } from "pinia";
 
 export const dimStore = defineStore("dimStore", () => {
+    const dimension = ref('intro_network')
     const root_nodes = ref(undefined)
     const root_links = ref(undefined)
     const deep_level = ref(0)
@@ -12,7 +13,13 @@ export const dimStore = defineStore("dimStore", () => {
     const depth = ref(2)
     const ontology_name_selected = ref('')
     const text_box_displayed = ref(false)
+    const dim_force_network_bool = ref(false)
 
+
+    function set_dimension(dimension_to_set) {
+        dimension.value = dimension_to_set
+    }
+    
     watch(() => w_data.value, (newValue, oldValue) => {
       })
 
@@ -24,6 +31,9 @@ export const dimStore = defineStore("dimStore", () => {
         ontology_left_position,
         depth,
         ontology_name_selected,
-        text_box_displayed }
+        text_box_displayed,
+        dimension,
+    
+        set_dimension}
 
 })

3e223e3c10e587e7b4ac9f59ba961806d5c4799d
pablo <paul7junior@gmail.com>
2024-11-16 11:31:08 -0700

    opacity transition reusable component

---
+++ OpacityTransition.vue
@@ -0,0 +1,26 @@
+<template>
+  <Transition
+    name="slide-up"
+    mode="out-in">
+    <slot></slot>
+  </Transition>
+</template>
+
+<script>
+</script>
+
+<style>
+.slide-up-enter-active,
+.slide-up-leave-active {
+  transition: all 0.2s ease-out;
+}
+
+.slide-up-enter-from {
+  opacity: 0;
+  
+}
+
+.slide-up-leave-to {
+  opacity: 0;
+}
+</style>
